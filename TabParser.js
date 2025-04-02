ea558de8d3beb07caeb9d874b23c5532ce5fd4e6
Jeff LeBoeuf <jeff@jeff.how>
2025-04-02 08:40:45 -0400

    Notes are now sorted by position

--- TabParser.js
+++ TabParser.js
@@ -7,15 +7,14 @@ let error;
 process.argv.forEach(function (val, index, array) {
   if (array.length < 5) {
     error = `You must supply the tab file, song name, and artist
-    example: > node tabparser.js allstar.tab "All Star" "Smash Mouth"`      
+    example: > node tabparser.js allstar.tab "All Star" "Smash Mouth"`;
   } else {
-    [_, _, fileArg, songArg, artistArg] = [...array]
+    [_, _, fileArg, songArg, artistArg] = [...array];
   }
-
-})
+});
 if (error) {
   console.log(error);
-  return
+  return;
 }
 
 fs.readFile(fileArg, "utf8", (err, data) => {
@@ -25,7 +24,7 @@ fs.readFile(fileArg, "utf8", (err, data) => {
   }
   //   console.log(data);
   const seconds = 3 * 60 + 20;
-  
+
   const tab = data.split("\n\n").map((group) => {
     // get the notes as data strings
     return group
@@ -41,12 +40,11 @@ fs.readFile(fileArg, "utf8", (err, data) => {
   const tabUnits = tab.reduce((a, c) => {
     return a + c[1].length;
   }, 0);
-  
 
   const tabs = Array(4).fill("");
-  tab.flat().forEach( (t,i) => {
-    tabs[i%4] += t
-  })
+  tab.flat().forEach((t, i) => {
+    tabs[i % 4] += t;
+  });
 
   const song = {
     name: songArg,
@@ -58,14 +56,13 @@ fs.readFile(fileArg, "utf8", (err, data) => {
   song.notes = tabs.reduce((a, c, i) => {
     const re = /\d(?:-\d)+/g; // find all hold notes
     const matches = c.matchAll(re);
-    
-      for (m of matches) {
-        // create hold note
-        a.push({ note: i, position: m.index, length: m[0].length });
-        // replace with # so it's not recreated as single note
-        c = c.replace(m, "#".repeat(m[0].length));
-      }
-    
+
+    for (m of matches) {
+      // create hold note
+      a.push({ note: i, position: m.index, length: m[0].length });
+      // replace with # so it's not recreated as single note
+      c = c.replace(m, "#".repeat(m[0].length));
+    }
 
     // create all single notes
     c.split("").forEach((n, j) => {
@@ -75,8 +72,9 @@ fs.readFile(fileArg, "utf8", (err, data) => {
       }
     });
     return a;
-  }, []);
+  }, []).sort((a,b)=> a.position - b.position);
+  
   const json = JSON.stringify(song, null, 2);
-    const filepath = song.name.toLowerCase().replaceAll(' ', '-') + '.json';
-    fs.writeFileSync(filepath, json);
+  const filepath = song.name.toLowerCase().replaceAll(" ", "-") + ".json";
+  fs.writeFileSync(filepath, json);
 });
